9dd8940692747f841b295ff850220c1d0834edd4
sebastian@alombra.de <sebastian@alombra.de@179f380e-4646-0410-8f9e-ad81fc4c133b>
2008-04-08 20:31:51 +0000

    added shell parameters added utf8 encoding

--- dinejs/src/de/alombra/dine/run/Runner.java
+++ dinejs/src/de/alombra/dine/run/Runner.java
@@ -1,35 +1,39 @@
 package de.alombra.dine.run;
 
-import de.alombra.dine.http.HttpManager;
-import de.alombra.dine.http.impl.HttpManagerImpl;
-import de.alombra.dine.steps.StepHolder;
-import de.alombra.dine.steps.execution.ExecutionQueue;
-import de.alombra.dine.steps.execution.StepExecutor;
-import de.alombra.dine.steps.execution.impl.BlockingExecutionQueue;
-import de.alombra.dine.steps.execution.impl.MultithreadedStepExecutor;
-import de.alombra.dine.steps.execution.instruction.impl.StepInstruction;
-import de.alombra.dine.steps.resolve.Resolver;
-import de.alombra.dine.steps.resolve.impl.FilesystemResolver;
 
 public class Runner {
 
 	public static void main( String[] args ) throws Exception {
 		
-		Resolver resolver = new FilesystemResolver("/Users/alombra/Entwicklung/workspace/dinejs/samples/immowelt");
-		
-		StepHolder stepHolder   = new StepHolder();
-		HttpManager httpManager = new HttpManagerImpl( 100 );
-		ExecutionQueue queue    = new BlockingExecutionQueue();
-		
-		stepHolder.instantiate( resolver );
-
-	  StepExecutor executor = new MultithreadedStepExecutor( 100,  queue, stepHolder, httpManager ); 
-	
-	  StepInstruction instruction = new StepInstruction();    
-	  instruction.setStepName( "/detail" );
-
-		executor.addInstruction( instruction );
-		
-		executor.start();		
+	  if ( args.length != 4 ) {
+	    
+	    System.err.println();
+	    System.err.println( "Usage: java -jar dinejs-all.jar <maxConcurrentThreads> <StepsBaseDirectory> <DownloadBaseDirectory> <seedStep>" );
+	    System.err.println();
+	    System.err.println( "e.g.: java -jar dinejs-all.jar 50 /home/myuser/steps /home/myuser/downloads /myStartStep" );
+	    System.err.println();
+	    
+	    System.exit( -1 );
+	  }
+	  
+	  String stepsDir = args[1];
+	  
+	  if ( stepsDir.endsWith( "/" ) )
+	    stepsDir = stepsDir.substring( 0, stepsDir.length()-1 );
+	  
+	  String downloadDir = args[2];
+	    
+	  if ( downloadDir.endsWith( "/" ) )
+	    downloadDir = downloadDir.substring( 0, downloadDir.length()-1 );
+	  
+	  
+	  RuntimeConfig.init( 
+	                  Integer.parseInt( args[0] ), 
+	                  stepsDir,  
+	                  downloadDir,
+	                  args[3] 
+	                 );
+	  
+	  Starter.setup();
 	}
 }
